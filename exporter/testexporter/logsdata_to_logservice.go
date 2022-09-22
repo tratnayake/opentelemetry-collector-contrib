0a1b808bccf3a25ab4fbb5c35fae5ef5a6d38cd9
Thilina Ratnayake <thilina.ratnayake@lightstep.com>
2022-09-22 09:43:36 -0700

    Attempt go mod init

--- exporter/testexporter/logsdata_to_logservice.go
+++ exporter/testexporter/logsdata_to_logservice.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/alibabacloudlogserviceexporter"
+package testexporter // import "github.com/tratnayake/opentelemetry-collector-contrib/exporter/testexporter"
 
 import (
 	"encoding/json"
